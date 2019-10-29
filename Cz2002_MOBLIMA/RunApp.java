9321a280c7e7f74c117d0bedb417f2038e55ba16
Zhang Xinyi <zh0031yi@e.ntu.edu.sg>
2019-10-29 20:21:18 +0800

    Changed structure

--- Cz2002_MOBLIMA/RunApp.java
+++ Cz2002_MOBLIMA/RunApp.java
@@ -1,8 +1,9 @@
-package root;
+import root.App;
 
 public class RunApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		App app = new App();
+//		app.run();
 	}
 }
