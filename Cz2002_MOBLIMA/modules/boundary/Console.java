6df171b7b655ce8130391b9042add3c043280f3e
Zhang Xinyi <zh0031yi@e.ntu.edu.sg>
2019-10-31 17:27:15 +0800

    Added console.getEmail function

--- Cz2002_MOBLIMA/modules/boundary/Console.java
+++ Cz2002_MOBLIMA/modules/boundary/Console.java
@@ -4,27 +4,34 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 public class Console {
     private Scanner sc;
-    private static final String ANSI_RED = "\u001B[31m";
-    private static final String ANSI_RESET = "\u001B[0m";
+    private final String ANSI_RED = "\u001B[31m";
+    private final String ANSI_RESET = "\u001B[0m";
+    private final Pattern EMAILPATTERN = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$");
     private String styleChar = "+";
+
     public Console() {
         sc = new Scanner(System.in);
     }
+
     public void log(String outputMsg) {
         System.out.println(outputMsg);
     }
+
     public void logWarning(String outputMsg) {
         System.out.println(ANSI_RED + outputMsg + ANSI_RESET);
     }
+
     public void logText(String outputMsg) {
         String repeated = new String(new char[outputMsg.length()]).replace("\0", styleChar);
         System.out.println(repeated);
         System.out.println(outputMsg);
         System.out.println(repeated);
     }
+
     public void logMenu(ArrayList<String> menuItems) {
         int i = 1;
         for (String str : menuItems) {
@@ -33,6 +40,7 @@ public class Console {
         }
         System.out.println(" ");
     }
+
     public void logWithSeparator(ArrayList<String> records, String separator) {
         for (String str : records) {
             StringTokenizer star = new StringTokenizer(str , separator);
@@ -43,6 +51,7 @@ public class Console {
         }
         System.out.println(" ");
     }
+
     public int getInt(String label, int lowerBound, int upperBound) {
         while (true) {
             try {
@@ -62,10 +71,24 @@ public class Console {
             }
         }
     }
+
     public String getStr(String title) {
         this.log(">> " + title + ": ");
         return sc.nextLine();
     }
+
+    public String getEmail() {
+        this.log(">> " + "Please enter your email" + ": ");
+        while (true){
+            String userInput = sc.nextLine();
+            if (EMAILPATTERN.matcher(userInput).matches()){
+                return userInput;
+            } else {
+                this.logWarning("Invalid input! Please try again.");
+            }
+        }
+    }
+    
     public void destoryScanner(){
         sc.close();
     }
