7618759788e0e8cbae9fddbd4df3cfd01a9b58e4
tang0413 <57039786+tang0413@users.noreply.github.com>
2019-11-01 16:25:19 +0800

    Revert "Show Time"
    This reverts commit f49c353f0ce31ecbf1ae818c9f1f52f74664cd66.

--- Cz2002_MOBLIMA/modules/entity/Cineplex.java
+++ Cz2002_MOBLIMA/modules/entity/Cineplex.java
@@ -17,8 +17,6 @@ public class Cineplex extends BaseEntity{
 
     public int getId() { return id; }
 
-    public void setCineplexName(String name) { this.cineplexName=cineplexName;}
-
     public String getCineplexName() { return cineplexName; }
 
 }
