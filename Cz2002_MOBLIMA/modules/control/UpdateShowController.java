e436a221c63d495ca414fd695e4fd699a08992d9
Zhang Xinyi <zh0031yi@e.ntu.edu.sg>
2019-11-03 21:59:11 +0800

    Demo commit

--- Cz2002_MOBLIMA/modules/control/UpdateShowController.java
+++ Cz2002_MOBLIMA/modules/control/UpdateShowController.java
@@ -1,4 +1,20 @@
 package modules.control;
 
-public class UpdateShowController {
+import modules.boundary.Console;
+
+public class UpdateShowController extends BaseController {
+    /**
+     * This is to instantiate a Controller object with I/O access
+     *
+     * @param inheritedConsole the Console instance passed down from the previous controller
+     */
+    public UpdateShowController(Console inheritedConsole) {
+        super(inheritedConsole);
+    }
+
+    @Override
+    @Deprecated
+    public void enter() {
+
+    }
 }
