7305b2635b62763416c252a92d226cb495664d8b
Zhang Xinyi <zh0031yi@e.ntu.edu.sg>
2019-11-06 20:55:08 +0800

    Update

--- Cz2002_MOBLIMA/modules/control/generalEnter.java
+++ Cz2002_MOBLIMA/modules/control/generalEnter.java
@@ -1,5 +1,8 @@
 package modules.control;
 
+/**
+ * This is to be implemented by controllers that specifically work for admin or common user
+ */
 public interface generalEnter {
     public void enter();
 }
--- Cz2002_MOBLIMA/modules/control/withAdminEnter.java
+++ Cz2002_MOBLIMA/modules/control/withAdminEnter.java
@@ -1,5 +1,8 @@
 package modules.control;
 
+/**
+ * This is to be implemented by controllers that performs differently for admin and common user
+ */
 public interface withAdminEnter {
     public void enter(Boolean isAdmin);
 }
